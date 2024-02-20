7f9c96190f51476bbe43a23cea180206970b5cde
BuildTools <januel.etienne@gmail.com>
2024-02-20 13:35:04 +0100

    +jobs enum

---
+++ src/utils/types/enums/Jobs.enum.ts
@@ -0,0 +1,10 @@
+export enum JOBS {
+    COOKER = "cooker",
+    FARMER = "farmer",
+    BUILDER = "builder",
+    DOCTOR = "doctor",
+    SCIENTIST = "scientist",
+    ENGINEER = "engineer",
+    ANIMALIST = "animalist",
+    GUARD = "guard"
+}
